d35eb29c652c31b82259115a5c14184ab8c39d4a
Tasuku <devbobcorn@outlook.com>
2023-07-17 11:59:04 +0800

    :sparkles: Support for display transforms

--- Assets/Scripts/Resource/ItemModelLoader.cs
+++ Assets/Scripts/Resource/ItemModelLoader.cs
@@ -20,7 +20,7 @@ namespace MinecraftClient.Resource
             this.manager = manager;
         }
 
-        // 
+        // Cached generated models
         private static Dictionary<int4, List<JsonModelElement>> generatedModels = new();
 
         public List<JsonModelElement> GetGeneratedItemModelElements(int layerCount, int precision, int thickness, bool useItemColor)
@@ -127,6 +127,7 @@ namespace MinecraftClient.Resource
 
                 bool containsTextures = modelData.Properties.ContainsKey("textures");
                 bool containsElements = modelData.Properties.ContainsKey("elements");
+                bool containsDisplay  = modelData.Properties.ContainsKey("display");
 
                 if (modelData.Properties.ContainsKey("parent"))
                 {
@@ -157,7 +158,8 @@ namespace MinecraftClient.Resource
                                 // This parent is already loaded as a block model, get it...
                                 parentModel = manager.BlockModelTable[parentIdentifier];
                             }
-                            else {
+                            else
+                            {
                                 // This parent is not yet loaded or is a generated model, load it...
                                 parentModel = LoadItemModel(parentIdentifier);
                                 parentIsGenerated = manager.GeneratedItemModels.Contains(parentIdentifier);
@@ -185,6 +187,12 @@ namespace MinecraftClient.Resource
                                 {
                                     model.Elements.Add(elem);
                                 }
+                            }
+
+                            // Inherit parent display transforms...
+                            foreach (var trs in parentModel.DisplayTransforms)
+                            {
+                                model.DisplayTransforms.Add(trs.Key, trs.Value);
                             }
 
                             break;
@@ -216,7 +224,6 @@ namespace MinecraftClient.Resource
                             model.Textures.Add(texItem.Key, texRef);
                         }
                     }
-
                 }
 
                 if (containsElements) // Discard parent elements and use own ones
@@ -225,6 +232,31 @@ namespace MinecraftClient.Resource
                     foreach (var elemItem in elemData)
                     {
                         model.Elements.Add(JsonModelElement.fromJson(elemItem));
+                    }
+                }
+
+                if (containsDisplay) // Add / Override display transforms...
+                {
+                    var trsData = modelData.Properties["display"].Properties;
+                    foreach (var trsItem in trsData)
+                    {
+                        var displayPos = DisplayPositionHelper.FromString(trsItem.Key);
+                        if (displayPos == DisplayPosition.Unknown)
+                        {
+                            Debug.LogWarning($"Unknown display position: {trsItem.Key}, skipping...");
+                            continue;
+                        }
+
+                        var displayTransform = VectorUtil.Json2DisplayTransform(trsItem.Value);
+
+                        if (model.DisplayTransforms.ContainsKey(displayPos)) // Override this display transform...
+                        {
+                            model.DisplayTransforms[displayPos] = displayTransform;
+                        }
+                        else // Add a new display transform...
+                        {
+                            model.DisplayTransforms.Add(displayPos, displayTransform);
+                        }
                     }
                 }
 
